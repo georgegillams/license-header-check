ed966994d59ca83c0b42cfe417fdf0ae5f693a14
George Gillams <george.gillams@skyscanner.net>
2019-02-26 12:34:53 +0000

    Add check for license presence

--- src/lib.js
+++ src/lib.js
@@ -4,8 +4,9 @@ const fs = require('fs');
 const path = require('path');
 const { execSync } = require('child_process');
 
-function checkCopyrightYears (codeFile, yearErrors, startYear, endYear, orgName) {
+function checkCopyrightStatements (codeFile, yearErrors, presenceErrors, startYear, endYear, orgName) {
   return new Promise(resolve => {
+    let licensePresent = false;
     if (fs.lstatSync(codeFile).isDirectory()) {
       resolve(null);
       return;
@@ -25,6 +26,7 @@ function checkCopyrightYears (codeFile, yearErrors, startYear, endYear, orgName)
         if (!lineMatch) {
           continue;
         }
+        licensePresent = true;
         const correctString = `${lineMatch[1]}Copyright `;
         if(startYear !== endYear){
           correctString += `${startYear}-`;
@@ -40,7 +42,38 @@ function checkCopyrightYears (codeFile, yearErrors, startYear, endYear, orgName)
         }
         yearErrors.push({codeFile, offendingLine: fileLines[i], expectedLine: correctString });
       }
+      if (!licensePresent) {
+        presenceErrors.push({codeFile});
+      }
       resolve(null);
+    });
+  });
+}
+
+function fixPresenceError (error) {
+  return new Promise(resolve => {
+    if (fs.lstatSync(error.codeFile).isDirectory()) {
+      resolve(null);
+      return;
+    }
+    fs.readFile(error.codeFile, 'utf8', (err, data) => {
+      if (err) {
+        resolve(err);
+        return;
+      }
+      if (!data) {
+        resolve(new Error('no data'));
+        return;
+      }
+
+      const newFileData = `LICENSE_HEADER_HERE\n\n${data}`
+
+      fs.writeFile(error.codeFile, newFileData, 'utf8', err2 => {
+        if (err2) {
+          resolve(err2);
+        }
+        resolve(null);
+      });
     });
   });
 }
@@ -74,29 +107,39 @@ function fixCopyrightYearError (error) {
   });
 }
 
-function createErrorMessage (yearErrors) {
+function createErrorMessage (yearErrors, presenceErrors) {
    let messages = [];
 
    if(yearErrors.length > 0){
      const yearErrorMessage = []
-     yearErrorMessage.push('Some copyright headers are wrong  😱');
+     yearErrorMessage.push('Some copyright header years are wrong  😱\n');
      yearErrors.forEach(error => {
-       yearErrorMessage.push(`\n${error.codeFile}`);
+       yearErrorMessage.push(`${error.codeFile}`);
      });
      messages.push(yearErrorMessage.join('\n'));
+   }
+
+   if(presenceErrors.length > 0){
+     const presenceErrorMessage = []
+     presenceErrorMessage.push('Some copyright headers are missing  😱\n');
+     presenceErrors.forEach(error => {
+       presenceErrorMessage.push(`${error.codeFile}`);
+     });
+     messages.push(presenceErrorMessage.join('\n'));
    }
 
   return messages.join('\n\n');
 }
 
-function performFixes (yearErrors) {
+function performFixes (yearErrors, presenceErrors) {
   return new Promise((resolve, reject) => {
-     const fixes = yearErrors.map(err => fixCopyrightYearError(err));
+    const fixes = yearErrors.map(err => fixCopyrightYearError(err));
+    presenceErrors.forEach(err => fixes.push(fixPresenceError(err)));
 
      Promise.all(fixes)
      .then(() => {
          const message = '\nAll fixed.  👍\n';
-         resolve(message); 
+         resolve(message);
          return;
      })
      .catch(() => {
@@ -108,41 +151,38 @@ function performFixes (yearErrors) {
 
 function runProcess (dir, startYear, endYear, orgName, shouldFix) {
   return new Promise((resolve, reject) => {
-   let yearErrors = [];
+    let yearErrors = [];
+    let presenceErrors = [];
 
-    let codeFiles = execSync(`(cd ${dir} && find * | grep -v node_modules)`)
+    let codeFiles = execSync(`(cd ${dir} && find * | grep -v node_modules | grep -v ".*gif" | grep -v ".*png" | grep -v ".*jpg")`)
       .toString()
       .split('\n');
 
     codeFiles = codeFiles.filter(s => s !== '');
     codeFiles = codeFiles.map(p => path.join(dir, p));
 
-    const checks = codeFiles.map(cf => checkCopyrightYears(cf, yearErrors, startYear, endYear, orgName));
+    const checks = codeFiles.map(cf => checkCopyrightStatements(cf, yearErrors, presenceErrors, startYear, endYear, orgName));
 
      Promise.all(checks)
        .then(() => {
-         if (yearErrors.length === 0) {
+         if (yearErrors.length + presenceErrors.length === 0) {
            const message = 'All good.  👍';
-           resolve(message); 
+           resolve(message);
            return;
          } else if (shouldFix) {
-           resolve(performFixes(yearErrors));
+           resolve(performFixes(yearErrors, presenceErrors));
            return;
          } else {
-           let message = 'Some copyright headers are wrong  😱\n';
-           yearErrors.forEach(error => {
-             message += `\n${error.codeFile}`;
-           });
+           let message = createErrorMessage(yearErrors, presenceErrors);
            reject(message);
            return;
          }
        })
        .catch(() => {
-         reject(`some yearErrors happened`);
+         reject(`Some errors happened. This is a problem with license-header-check, and not your code!`);
          return;
        });
   });
 }
 
 module.exports = runProcess;
-
